8ff9c5e8762a2869100b698ac219e8d7c23b3c74
hruedisser <hruedisser@know-center.at>
2021-09-22 21:17:59 +0200

    update workshop material

--- windows.py
+++ windows.py
@@ -1,44 +1,6 @@
 import pandas as pds
-import datetime
 import numpy as np
-import time
-import event as evt
-import numpy as np
-import math
 from numpy.lib.stride_tricks import as_strided
-
-
-'''
-Utilities used for transforming a dataset in sliding window in a smart way,
-less memory costing than what was done for the sliding windows
-'''
-
-
-def windowed(X, window):
-    '''
-    Using stride tricks to create a windowed view on the original
-    data.
-    '''
-    shape = int((X.shape[0] - window) + 1), window, X.shape[1]
-    strides = (X.strides[0],) + X.strides
-    X_windowed = np.lib.stride_tricks.as_strided(X, shape=shape, strides=strides)
-    return X_windowed
-
-def windowedU(X, window):
-    n = len(X)/window
-    if n == math.trunc(n):
-        a = np.array_split(X, n)
-        X_windowed = [x for x in a]    
-    else:
-        a = np.array_split(X, np.ceil(n))
-        X_windowed = [x for x in a if len(x) == window]
-        for x in a:
-            print(len(x))
-    return X_windowed
-
-def windowedUnet(X,window):
-    X_windowed = [X[x:x+window] for x in range(0, len(X)-window, window)]
-    return np.asarray(X_windowed)
 
 def make_views(arr,win_size,step_size,writeable = False):
     """
